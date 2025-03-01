31b62d77fc45270cef18c1eb0d8ded6c27a11386
John <johnkesterson@johns-MBP.home.local>
2025-03-01 14:48:53 -0500

    1st push

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 // develop
 //John Branch
+//Boiler plate
+const mongoose = require("mongoose")
 const express = require("express");
 const app = express();
+
+//importing controllers
+const messageController = require ("./controllers/message.controller")
+const roomController = require ("./controllers/room.controller")
+const userController = require ("./controllers/user.controller")
+
+//connecting to database
+//! change link 
+mongoose.connect("mongodb://localhost:27017/instapet-db")
+const db = mongoose.connection 
+db.on("error", console.error.bind(console, "Connection error"));
+
+//middleware
+app.use(express.json());
+
+//controller routes
+app.use("/message", messageController)
+app.use("/room", roomController)
+app.use("/user", userController)
 
 const PORT = 4000;
 
--- model/room.model.js
+++ model/room.model.js
@@ -1,1 +1,20 @@
 //room model
+
+//importing mongoose
+const mongoose = require("mongoose");
+
+const RoomSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    addedUsers: {
+        type: String,
+        required: true
+    }
+})
+module.exports = mongoose.model("Room", RoomSchema)
