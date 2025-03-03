d22747fff57a7fe4c7d7d6a016f8d80fab0e3b93
Nicholas Russotti <test@test.com>
2025-03-03 18:17:25 -0500

    nick 2

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -1,8 +1,20 @@
 //message controller
+const router = require("express").Router();
 
-{
-    "when": "2018-07-15T20:00:47.696Z",
-    "user": "John",
-    "room": "Main",
-    "body": "I really want to attend NASA's DEVELOP program this summer!"
-}
+const Message = require("../model/message.model");
+
+// const validateSession = require("../middleware/validate-session")
+
+// add message route
+// Request type: POST
+// http://localhost:4000/message/add
+
+router.post("/add", async (req, res) => {
+  try {
+    res.json({ message: `route works` });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+module.exports = router;
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,1 +1,37 @@
 //user controller
+const router = require("express").Router();
+const User = require("../model/user.model");
+
+// import jsonwebtoken for creating a token
+const jwt = require("jsonwebtoken");
+// import bcrypt for password hashing
+const bcrypt = require("bcrypt");
+
+// Endpoint: "http://localhost:4000/user/register"
+// Request Type: POST
+router.post("/register", async (req, res) => {
+  try {
+    // Destructure
+    const { firstName, lastName, email, password } = req.body;
+
+    // Create  variable and use the User model to create a new user
+    const user = new User({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: bcrypt.hashSync(password, 10), //hashing the password
+    });
+
+    //  Save the new user in the database and store the response in a variable
+    const newUser = await user.save();
+
+    // 5. Create a token - three parameters - (what you want stored, secret_word, options like expiring)
+    const token = jwt.sign({ id: newUser._id }, "secret", { expiresIn: "7d" });
+
+    res.json({ message: `route works`});
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+module.exports = router;
